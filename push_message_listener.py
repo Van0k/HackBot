d5d2b9a2ed3795e9c8dc3573d8054e79c0b1c84f
Иван Камакин <ivkamakin@edu.hse.ru>
2018-05-17 13:33:14 +0300

    Small fixes to push notification listener

--- push_message_listener.py
+++ push_message_listener.py
@@ -11,25 +11,26 @@ def periodic(interval, action, actionargs=()):
     action(*actionargs)
 
 def get_messages_to_send(endpoint):
-    r = requests.get(endpoint + '?lastMessageTimestamp=%d&eventId=2' % int(time.time()))
-    print(r.text)
+    r = requests.get(endpoint + '?eventId=1')
     resp = r.json()
     messages = resp
     print(messages)
     return messages
 
 def send_messages(messages, config, bot):
-    currentMessageCount = 0
-    for message in messages['messages']:
-        for receiver in messages['receivers']:
-            chat_id = config['users'][str(receiver)]['chat_id']
-            bot.send_message(chat_id=chat_id, text=message['content'])
-            currentMessageCount += 1
-            if currentMessageCount > 10:
-                time.sleep(1)
-                currentMessageCount = 0
-    message_numbers = [message['id'] for message in messages['messages']]
-    config['latest_msg'] = max(message_numbers)
+    print(config['users'])
+    if messages['messages']:
+        currentMessageCount = 0
+        for message in messages['messages']:
+            for receiver in messages['receivers']:
+                chat_id = config['users'][str(receiver)]['chat_id']
+                bot.send_message(chat_id=chat_id, text=message['content'])
+                currentMessageCount += 1
+                if currentMessageCount > 10:
+                    time.sleep(1)
+                    currentMessageCount = 0
+        message_numbers = [message['id'] for message in messages['messages']]
+        config['latest_msg'] = max(message_numbers)
 
 def get_and_send_messages(endpoint, config, bot):
     messages = get_messages_to_send(endpoint)
